afd7f596de96a195e741bbf28f19f8d4f31686c4
Tomáš Novák <hello@tom-novak.com>
2020-07-12 15:40:33 +0200

    Draw stave lines prototype

--- lib/src/widgets/Stave.dart
+++ lib/src/widgets/Stave.dart
@@ -11,14 +11,36 @@ class Stave extends StatelessWidget {
 }
 
 class StavePainter extends CustomPainter {
+  static const lineWidth = 5.0;
+  static const spaceWidth = 15.0;
+
+  final Paint linePaint = Paint();
+
+  StavePainter() {
+    linePaint
+      ..style = PaintingStyle.stroke
+      ..color = Color.fromARGB(255, 255, 255, 255)
+      ..strokeCap = StrokeCap.square;
+  }
+
+  double get containerWidth => 50.0;
 
   @override
   void paint(Canvas canvas, Size size) {
-    //canvas.drawLine(p1, p2, paint);
+    List<Offset> offsets;
+    for (int i = 0; i <= 5; i++) {
+      offsets = _lineOffsets(i);
+      canvas.drawLine(offsets[0], offsets[1], linePaint);
+    }
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return true;
+  }
+
+  List<Offset> _lineOffsets(int line) {
+    var vertical = (lineWidth + spaceWidth) * line;
+    return [Offset(0.0, vertical), Offset(containerWidth, vertical)];
   }
 }
